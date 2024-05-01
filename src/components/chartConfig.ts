418584d6c010af73ee569b78685a85434d43b206
Vladimir <myndcontrol88@gmail.com>
2024-05-01 17:01:24 +0700

    Uploaded project

---
+++ src/components/chartConfig.ts
@@ -0,0 +1,14 @@
+export const data = {
+    labels: ['1', '2', '3', '4'],
+    datasets: [
+        {
+            backgroundColor: ['green', 'blue', 'cyan', 'red'],
+            data: [40, 20, 80, 10]
+        }
+    ]
+}
+
+export const options = {
+    responsive: true,
+    maintainAspectRatio: false
+}
